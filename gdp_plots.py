6be90b7ee5d3e271c43ff5fd99c7be4df3bcb2e1
taomadankui <mtao4@wisc.edu>
2018-08-30 15:03:11 -0500

    set -a flag to plot all continent data files

--- gdp_plots.py
+++ gdp_plots.py
@@ -1,21 +1,28 @@
+# this script reads in continent files and plot 
+# Usage:
+# python gdp_plots.py (-a/ *.csv)
+
 import pandas
 # we need to import part of matplotlib
 # because we are no longer in a notebook
 import matplotlib.pyplot as plt
 # enable command line argument
 import sys
-print(sys.argv[1])
+#print(sys.argv[1])
+
+import glob # getting a list of files
 
 # load data and transpose so that country names are
 # the columns and their gdp data becomes the rows
 
-# read data into a pandas dataframe and transpose
-filename_list = sys.argv[1:] # take up all arguments after the pythonfile
-
-# Usage:
-# python gdp_plots.py *.csv
+# reads in the arguments as -a or filename
+if sys.argv[1]=='-a':
+    filename_list = glob.glob('*gdp*.csv')
+else:
+    filename_list = sys.argv[1:]
 
 for filename in filename_list:
+    # read data into a pandas dataframe and transpose
     data = pandas.read_csv(filename, index_col = 'country').T
 
     # create a plot the transposed data
